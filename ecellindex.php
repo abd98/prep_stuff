3932c62bb40e1f9f0ac3a42bf4d4422cd95f5111
Daipayan Mukherjee <30791985+Satyr09@users.noreply.github.com>
2017-10-22 00:49:06 +0530

    Add files via upload

---
+++ ecellindex.php
@@ -0,0 +1,275 @@
+<? php
+$name ='Daipayan Mukherjee';
+
+?>
+
+<html>
+<head>
+
+<meta name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="assets/styles.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      
+      <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+      
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="slick/slick.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+  
+    </head>
+
+<body>
+<header class="main_h">
+
+    <div class="roww">
+        <a class="logo" href="#"><img src="assets/images/main_logo.png"  style="height: 50px"/>
+      </a>
+
+        <div class="mobile-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <nav>
+            <ul>
+                <li><p href="#">ABOUT</p></li>
+                <li><p href="#">CONTACT</p></li>
+                <li><p href="#">THE TEAM</p></li>
+                <li><p href="#">APPLY</p></li>
+            </ul>
+        </nav>
+
+    </div> 
+
+</header>
+
+
+
+
+<div class="page">
+    <div class="section jumbo">
+        <div class="logos">
+            <img src="main_logo.png"/>
+            <img src= "main_logo2.png"/>
+        </div>
+        <div class="headingz">
+            <span style="font-size: 60px"> JADAVPUR UNIVERSITY<br> E CELL</span>
+        </div>
+    </div>
+
+
+    <div class="section features">
+        <div class="feature feature-1">
+
+            
+        <div id="tests" class="hvr-ripple-out"><i class="fa fa-lightbulb-o fa-3x" aria-hidden="true" style="color:black"></i>
+</div>
+        <div><h1>IDEATE</h1></div>
+        <div><p >Lorem ipsum dollis nostra aut,<br> t praesent dolor magna risus.</p></div>
+
+        </div>
+        <div class="feature feature-2">
+        <div id="tests" class="hvr-ripple-out"><i class="fa fa-users fa-3x" aria-hidden="true"></i>
+</div>
+        <div><h1>INNOVATE</h1></div>
+        <div><p>Lorem ipsum dolonostra aut,<br> dolor magna risus. Dui sceleris</p></div>
+        </div>
+        <div class="feature feature-3">
+        <div id="tests" class="hvr-ripple-out"><i class="fa fa-cogs fa-3x" aria-hidden="true"></i>
+</div>
+        <div><h1>BUILD</h1></div>
+        <div><p >Lorem ipsum dolor s et rhonrrmat<br> praesr magna risus. Dui scelerisque </p></div>
+
+        </div>
+    </div>
+
+
+
+    <div class="section" style="text-align: center" id="upcoming">
+
+    <h1 style="color: white "> UPCOMING EVENTS </h1>
+</div>
+
+
+<div id="test" style="color: white"> 
+<img src="http://www.hultprizesix.com/images/logo-stack.png"  id="recentimg" />
+
+</div>
+  
+
+
+<div class="section" style="text-align: center" id="upcoming" >
+
+    <h1 style="color: white "> PAST EVENTS </h1>
+
+<ul id="hexgrid"> 
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="http://www.idolator.com/wp-content/uploads/sites/10/2013/05/13/daft-punk-random-access-memories.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="assets/images/hult-event.jpg" alt="" />
+            <h1>Hult Prize 2016</h1>
+            <p>JU E-Cell hosted Hult Prize 2016 on campus.
+              Team Hope emerged as winers and went on to the national finals
+            </p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="assets/images/hult-event.jpg" alt="" />
+            <h1>Hackathon</h1>
+            <p>A Hackathon organised by Facebook Dev Circles and JUB E-Cell</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="http://www.idolator.com/wp-content/uploads/sites/10/2013/05/13/daft-punk-random-access-memories.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="http://www.idolator.com/wp-content/uploads/sites/10/2013/05/13/daft-punk-random-access-memories.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="http://www.idolator.com/wp-content/uploads/sites/10/2013/05/13/daft-punk-random-access-memories.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="assets/images/abc-event.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="http://www.idolator.com/wp-content/uploads/sites/10/2013/05/13/daft-punk-random-access-memories.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+      <li class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img src="http://www.idolator.com/wp-content/uploads/sites/10/2013/05/13/daft-punk-random-access-memories.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </div>
+      </li>
+
+
+
+    </ul>
+</div>
+
+
+<div class='application-container' id="daaamn">
+<div class='application'>
+  <div class='about'><h1 style="  text-shadow: 0 1px 0 #ccc,
+               0 2px 0 #c9c9c9,
+               0 3px 0 #bbb,
+               0 4px 0 #b9b9b9,
+               0 5px 0 #aaa,
+               0 6px 1px rgba(0,0,0,.1),
+               0 0 5px rgba(0,0,0,.1),
+               0 1px 3px rgba(0,0,0,.3),
+               0 3px 5px rgba(0,0,0,.2),
+               0 5px 10px rgba(0,0,0,.25),
+               0 10px 10px rgba(0,0,0,.2),
+               0 20px 20px rgba(0,0,0,.15) ; color: white ; ">ABOUT US</h1>
+  <p >
+  An Entrepreneurship Development Cell has been created as a joint initiative of university students and the Business Incubation Centre, Jadavpur University, to nurture and inspire the creative minds of the students in the university by providing them the opportunities to showcase their ideas and helping them in turning their ideas into entrepreneurial ventures. The initiative to form an Entrepreneurship Development Cell started in 2016. The Cell was formed on 20th July, 2016 through a workshop held in Salt Lake Campus in presence of Prof. Suranjan Das, Vice Chancellor, Jadavpur University, Prof. Asis Mazumdar, Member Secretary, Business Incubation Centre (MSME), Jadavpur University, several other distinguished faculty members and officers of the university and other well-known personalities in the start-up community such as Mr. Aloke Mookherjea, Former Chairman, Confederation of Indian Industry (ER) and Mr. Ravi Ranjan, Sr. Associate, NASSCOM 10,000 Start-ups. 
+</p>
+
+
+    <span id= "counter" class="counter-value" data-count="500" style="font-size: 25px">0</span>
+    <span id="counter"  class="counter-value" data-count="50" style="font-size: 25px">0</span>
+    <span id="counter" class="counter-value" data-count="75" style="font-size: 25px">0</span>
+    <span id="counter" class="counter-value" data-count="5" style="font-size: 25px">0</span>
+    </div>
+
+  <div class='apply'><h1>APPLY</h1>
+  <button type = "button" class = "btn btn-info btn-lg hvr-float-shadow">Apply</button></div>
+  
+</div>
+</div>
+
+
+
+</div>
+
+</div>
+
+<div id="section pages">
+<div class="rowslide">
+  <div class="col sm-11 small-centered">
+    <div class="slider slider-single">
+      <div><h3>1</h3></div>
+      <div><h3>2</h3></div>
+      <div><h3>3</h3></div>
+      <div><h3>4</h3></div>
+      <div><h3>5</h3></div>
+      <div><h3>6</h3></div>
+      <div><h3>7</h3></div>
+      <div><h3>8</h3></div>
+      <div><h3>9</h3></div>
+      <div><h3>10</h3></div>
+    </div>
+    <div class="slider slider-nav">
+      <div><h3><span>1</span></h3></div>
+      <div><h3><span>2</span></h3></div>
+      <div><h3><span>3</span></h3></div>
+      <div><h3><span>4</span></h3></div>
+      <div><h3><span>5</span></h3></div>
+      <div><h3><span>6</span></h3></div>
+      <div><h3><span>7</span></h3></div>
+      <div><h3><span>8</span></h3></div>
+      <div><h3><span>9</span></h3></div>
+      <div><h3><span>10</span></h3></div>
+    </div>
+  </div>
+</div>
+</div>
+  
+
+<a href="javascript:void(0);" id="scroll" title="Scroll to Top" style="display: none;"><span></span></a>
+
+
+
+</body>
+</html>
